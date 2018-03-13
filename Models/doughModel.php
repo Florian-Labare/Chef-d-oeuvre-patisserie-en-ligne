a16667267d2d9808dec5bc19d496b29ee72c02c2
Florian LABARE <flabare@gmail.com>
2018-03-13 23:16:54 +0100

    model pâtes terminé

--- Models/doughModel.php
+++ Models/doughModel.php
@@ -31,15 +31,86 @@ class Doughs
     $pdo_statement = self::createStatement($sql);
 
     if (
-    $pdo_statement &&
-    $pdo_statement->bindParam(
+       $pdo_statement &&
+       $pdo_statement->bindParam(
         ':base', htmlspecialchars($values['base'])
     ){
 
-    $ok = true;
+      $ok = true;
 
      }
     return $ok;
   }
 
+  public static function readAll()
+  {
+    $sql = 'SELECT * FROM doughs deleted_at IS NULL';
+
+    $doughs = [];
+
+    $pdo_statement = self::createStatement($sql);
+
+    if ($pdo_statement && $pdo_statement->execute()) {
+    $doughs = $pdo_statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+     return $doughs;
+  }
+
+  public static function read($id)
+  {
+    $sql = 'SELECT * FROM doughs WHERE id=:id AND deleted_at IS NULL';
+
+    $doughs = null;
+
+    $pdo_statement = self::createStatement($sql);
+
+     if (
+        $pdo_statement &&
+        $pdo_statement->bindParam(':id', $id, PDO::PARAM_INT) &&
+        $pdo_statement->execute()
+    ){
+        $doughs = $pdo_statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    return $doughs;
+  }
+
+ public static function update($id, $values)
+ {
+   $sql = 'UPDATE doughs SET base=:base WHERE id=:id';
+
+      $ok = false;
+
+   $pdo_statement = self::createStatement($sql);
+
+   if (
+      $pdo_statement &&
+      $pdo_statement->bindParam(':id', $id, PDO::PARAM_INT) &&
+      $pdo_statement->bindParam(':base', htmlspecialchars($values['base'])
+      )
+    ){
+      $ok = true;
+    }
+
+    return $ok;
+ }
+
+ public static function delete($id)
+ {
+   $sql ='UPDATE doughs SET deleted_at=CURRENT_TIMESTAMP() WHERE id=:id';
+
+      $ok = false;
+
+   $pdo_statement = self::createStatement($sql);
+
+   if (
+      $pdo_statement &&
+      $pdo_statement->bindParam(':id', $id, PDO::PARAM_INT) &&
+      $pdo_statement->execute()
+  ){
+      $ok = true;
+  }
+    return $ok;
+ }
 }
