ac4d974f12a09722fdfb4cb9ae535dd391183597
Florian LABARE <flabare@gmail.com>
2018-03-13 15:56:51 +0100

    ajout model pâte

---
+++ Model/doughModel.php
@@ -0,0 +1,25 @@
+<?php
+ 
+class Dough
+{
+
+  private static function createStatement($sql)
+  {
+    try {
+    require './config.php'
+
+    $pdo = new PDO(
+       "mysql:dbname=$dbname;host=$host;charset=utf8", $user, $password
+     );
+
+    $pdo_statement = $pdo->preapre($sql);
+   } catch (PDOException $e) {
+
+           echo 'erreur : ' . $e->getMessage();
+
+           $pdo_statement = null;
+        }
+    return $pdo_statement;
+  }
+
+}
