f86583c5489f37f1e52c5973c01f437fb9af13b2
Florian LABARE <flabare@gmail.com>
2018-03-15 01:47:10 +0100

    continuation de bread cream

--- Models/creamModel.php
+++ Models/creamModel.php
@@ -20,4 +20,59 @@ class Creams
          }
      return $pdo_statement;
    }
+
+
+  public static function create($name)
+  {
+    $sql = 'INSERT INTO creams (name) VALUES (:name)';
+
+      $ok = false;
+
+    $pdo_statement = self::createStatement($sql);
+
+    if (
+       $pdo_statement &&
+       $pdo_statement->bindParam(':name', $name) &&
+       $pdo_statement->execute()
+    ){
+
+       $ok = true;
+
   }
+    return $ok;
+ }
+
+  public static function readAll()
+  {
+    $sql = 'SELECT * FROM creams WHERE deleted_at IS NULL';
+
+    $creams = [];
+
+    $pdo_statement = self::createStatement($sql);
+
+  if ($pdo_statement && $pdo_statement->execute()) {
+    $doughs = $pdo_statement->fetchAll(PDO::FETCH_ASSOC);
+   }
+
+    return $creams;
+ }
+
+  public static function read($id)
+  {
+    $sql = 'SELECT * FROM creams WHERE id=:id AND deleted_at IS NULL';
+
+    $creams = null;
+
+    $pdo_statement = self::createStatement($sql);
+
+  if (
+       $pdo_statement &&
+       $pdo_statement->bindParam(':id', $id, PDO::PARAM_INT) &&
+       $pdo_statement->execute()
+   ){
+       $doughs = $pdo_statement->fetch(PDO::FETCH_ASSOC);
+   }
+
+   return $creams;
+ }
+}
